5973a55de08e134bfdd0e51404ad076fb752f7c8
mrwhale <harrysibenaler@gmail.com>
2015-01-04 20:59:55 +1100

    Changed to white commands, merged white-lights branch into my master. Next step is creating a list view so i can have both rgbw and white at the swipe of a finger. but thats gonna take alot of time :S

--- LC1/src/main/java/tv/piratemedia/lightcontroler/DataLayerListenerService.java
+++ LC1/src/main/java/tv/piratemedia/lightcontroler/DataLayerListenerService.java
@@ -20,50 +20,50 @@ public class DataLayerListenerService extends WearableListenerService {
         cmd = new controlCommands(this, mCont.mHandler);
         switch (messageEvent.getPath()){
             case "/1":
-                if (cmd.appState.getOnOff(1) == false) {
-                    cmd.LightsOn(1);
-                    cmd.appState.setOnOff(1, true);
+                if (cmd.appState.getOnOff(5) == false) {
+                    cmd.LightsOn(5);
+                    cmd.appState.setOnOff(5, true);
                     Log.d(TAG, "lights in zone 1 on");
                 }
-                else if (cmd.appState.getOnOff(1) == true){
-                    cmd.LightsOff(1);
-                    cmd.appState.setOnOff(1, false);
+                else if (cmd.appState.getOnOff(5) == true){
+                    cmd.LightsOff(5);
+                    cmd.appState.setOnOff(5, false);
                     Log.d(TAG, "lights in zone 1 off");
                 }
                 break;
             case "/2":
-                if (cmd.appState.getOnOff(2) == false) {
-                    cmd.LightsOn(2);
-                    cmd.appState.setOnOff(2, true);
+                if (cmd.appState.getOnOff(6) == false) {
+                    cmd.LightsOn(6);
+                    cmd.appState.setOnOff(6, true);
                     Log.d(TAG, "lights in zone 2 on");
                 }
-                else if (cmd.appState.getOnOff(2) == true){
-                    cmd.LightsOff(2);
-                    cmd.appState.setOnOff(2, false);
+                else if (cmd.appState.getOnOff(6) == true){
+                    cmd.LightsOff(6);
+                    cmd.appState.setOnOff(6, false);
                     Log.d(TAG, "lights in zone 2 off");
                 }
                 break;
             case "/3":
-                if (cmd.appState.getOnOff(3) == false) {
-                    cmd.LightsOn(3);
-                    cmd.appState.setOnOff(3, true);
+                if (cmd.appState.getOnOff(7) == false) {
+                    cmd.LightsOn(7);
+                    cmd.appState.setOnOff(7, true);
                     Log.d(TAG, "lights in zone 3 on");
                 }
-                else if (cmd.appState.getOnOff(3) == true){
-                    cmd.LightsOff(3);
-                    cmd.appState.setOnOff(3, false);
+                else if (cmd.appState.getOnOff(7) == true){
+                    cmd.LightsOff(7);
+                    cmd.appState.setOnOff(7, false);
                     Log.d(TAG, "lights in zone 3 off");
                 }
                 break;
             case "/4":
-                if (cmd.appState.getOnOff(4) == false) {
-                    cmd.LightsOn(4);
-                    cmd.appState.setOnOff(4, true);
+                if (cmd.appState.getOnOff(8) == false) {
+                    cmd.LightsOn(8);
+                    cmd.appState.setOnOff(8, true);
                     Log.d(TAG, "lights in zone 4 on");
                 }
-                else if (cmd.appState.getOnOff(4) == true){
-                    cmd.LightsOff(4);
-                    cmd.appState.setOnOff(4, false);
+                else if (cmd.appState.getOnOff(8) == true){
+                    cmd.LightsOff(8);
+                    cmd.appState.setOnOff(8, false);
                     Log.d(TAG, "lights in zone 4 off");
                 }
                 break;
