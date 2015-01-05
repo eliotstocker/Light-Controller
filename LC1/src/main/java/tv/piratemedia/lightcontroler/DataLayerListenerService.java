8575180d9fb21321ddcff2e88e3e891ba362d217
mrwhale <harrysibenaler@gmail.com>
2015-01-05 21:09:45 +1100

    Changed to wearable list view. which is a scrollable view, and you can click on the items. that means i have included all zones rgbw 1-4 and white 1-4

--- LC1/src/main/java/tv/piratemedia/lightcontroler/DataLayerListenerService.java
+++ LC1/src/main/java/tv/piratemedia/lightcontroler/DataLayerListenerService.java
@@ -9,68 +9,110 @@ import com.google.android.gms.wearable.WearableListenerService;
 public class DataLayerListenerService extends WearableListenerService {
 
     private static final String TAG = "DataLayer";
-    private controller mCont = new controller();
-    controlCommands cmd;
 
-    public void onCreate(){
-        Log.d(TAG, "Just created new control commands");
-        cmd = new controlCommands(this, mCont.mHandler);
-    }
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         super.onMessageReceived(messageEvent);
         // if ("/MESSAGE".equals(messageEvent.getPath())) {
         Log.d(TAG, "message received" + messageEvent.getPath());
-       // controller mCont = new controller();
-        //controlCommands cmd;
-        //cmd = new controlCommands(this, mCont.mHandler);
+        controller mCont = new controller();
+        controlCommands cmd;
+        cmd = new controlCommands(this, mCont.mHandler);
         switch (messageEvent.getPath()){
-            case "/1":
+            case "/0":
                 if (!cmd.appState.getOnOff(5)) {
-                    cmd.LightsOn(5);
+                    //cmd.LightsOn(5);
                     cmd.appState.setOnOff(5, true);
-                    Log.d(TAG, "lights in zone 1 on");
+                    Log.d(TAG, "lights in w.zone 1 on");
                 }
                 else if (cmd.appState.getOnOff(5)){
-                    cmd.LightsOff(5);
+                    //cmd.LightsOff(5);
                     cmd.appState.setOnOff(5, false);
-                    Log.d(TAG, "lights in zone 1 off");
+                    Log.d(TAG, "lights in w.zone 1 off");
+                }
+                break;
+            case "/1":
+                if (!cmd.appState.getOnOff(6)) {
+                    //cmd.LightsOn(6);
+                    cmd.appState.setOnOff(6, true);
+                    Log.d(TAG, "lights in w.zone 2 on");
+                }
+                else if (cmd.appState.getOnOff(6)){
+                    //cmd.LightsOff(6);
+                    cmd.appState.setOnOff(6, false);
+                    Log.d(TAG, "lights in w.zone 2 off");
                 }
                 break;
             case "/2":
-                if (!cmd.appState.getOnOff(6)) {
-                    cmd.LightsOn(6);
-                    cmd.appState.setOnOff(6, true);
-                    Log.d(TAG, "lights in zone 2 on");
+                if (!cmd.appState.getOnOff(7)) {
+                    //cmd.LightsOn(7);
+                    cmd.appState.setOnOff(7, true);
+                    Log.d(TAG, "lights in w.zone 3 on");
                 }
-                else if (cmd.appState.getOnOff(6)){
-                    cmd.LightsOff(6);
-                    cmd.appState.setOnOff(6, false);
-                    Log.d(TAG, "lights in zone 2 off");
+                else if (cmd.appState.getOnOff(7)){
+                    //cmd.LightsOff(7);
+                    cmd.appState.setOnOff(7, false);
+                    Log.d(TAG, "lights in w.zone 3 off");
                 }
                 break;
             case "/3":
-                if (!cmd.appState.getOnOff(7)) {
-                    cmd.LightsOn(7);
-                    cmd.appState.setOnOff(7, true);
-                    Log.d(TAG, "lights in zone 3 on");
+                if (!cmd.appState.getOnOff(8)) {
+                    //cmd.LightsOn(8);
+                    cmd.appState.setOnOff(8, true);
+                    Log.d(TAG, "lights in w.zone 4 on");
                 }
-                else if (cmd.appState.getOnOff(7)){
-                    cmd.LightsOff(7);
-                    cmd.appState.setOnOff(7, false);
-                    Log.d(TAG, "lights in zone 3 off");
+                else if (cmd.appState.getOnOff(8)){
+                    //cmd.LightsOff(8);
+                    cmd.appState.setOnOff(8, false);
+                    Log.d(TAG, "lights in w.zone 4 off");
                 }
                 break;
             case "/4":
-                if (!cmd.appState.getOnOff(8)) {
-                    cmd.LightsOn(8);
-                    cmd.appState.setOnOff(8, true);
-                    Log.d(TAG, "lights in zone 4 on");
+                if (!cmd.appState.getOnOff(1)) {
+                    //cmd.LightsOn(1);
+                    cmd.appState.setOnOff(1, true);
+                    Log.d(TAG, "lights in rgbw zone 1 on");
                 }
-                else if (cmd.appState.getOnOff(8)){
-                    cmd.LightsOff(8);
-                    cmd.appState.setOnOff(8, false);
-                    Log.d(TAG, "lights in zone 4 off");
+                else if (cmd.appState.getOnOff(1)){
+                    //cmd.LightsOff(1);
+                    cmd.appState.setOnOff(1, false);
+                    Log.d(TAG, "lights in rgbw zone 1 off");
+                }
+                break;
+            case "/5":
+                if (!cmd.appState.getOnOff(2)) {
+                    //cmd.LightsOn(2);
+                    cmd.appState.setOnOff(2, true);
+                    Log.d(TAG, "lights in rgbw zone 2 on");
+                }
+                else if (cmd.appState.getOnOff(2)){
+                    //cmd.LightsOff(2);
+                    cmd.appState.setOnOff(2, false);
+                    Log.d(TAG, "lights in rgbw zone 2 off");
+                }
+                break;
+            case "/6":
+                if (!cmd.appState.getOnOff(3)) {
+                    //cmd.LightsOn(3);
+                    cmd.appState.setOnOff(3, true);
+                    Log.d(TAG, "lights in rgbw zone 3 on");
+                }
+                else if (cmd.appState.getOnOff(3)){
+                    //cmd.LightsOff(3);
+                    cmd.appState.setOnOff(3, false);
+                    Log.d(TAG, "lights in rgbw zone 3 off");
+                }
+                break;
+            case "/7":
+                if (!cmd.appState.getOnOff(4)) {
+                    //cmd.LightsOn(4);
+                    cmd.appState.setOnOff(4, true);
+                    Log.d(TAG, "lights in rgbw zone 4 on");
+                }
+                else if (cmd.appState.getOnOff(4)){
+                    //cmd.LightsOff(4);
+                    cmd.appState.setOnOff(4, false);
+                    Log.d(TAG, "lights in rgbw zone 4 off");
                 }
                 break;
         }
--- wear/src/main/java/tv/piratemedia/lightcontroler/MainActivity.java
+++ wear/src/main/java/tv/piratemedia/lightcontroler/MainActivity.java
@@ -1,4 +1,157 @@
+
+//http://www.technotalkative.com/android-wear-part-5-wearablelistview/
 package tv.piratemedia.lightcontroler;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements WearableListView.ClickListener{
+
+    private WearableListView mListView;
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mListView = (WearableListView) stub.findViewById(R.id.listView1);
+                mListView.setAdapter(new MyAdapter(MainActivity.this));
+                mListView.setClickListener(MainActivity.this);
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    private static ArrayList<String> listItems;
+    static {
+        listItems = new ArrayList<String>();
+        listItems.add("White Zone 1");
+        listItems.add("White Zone 2");
+        listItems.add("White Zone 3");
+        listItems.add("White Zone 4");
+        listItems.add("RGBW Zone 1");
+        listItems.add("RGBW Zone 2");
+        listItems.add("RGBW Zone 3");
+        listItems.add("RGBW Zone 4");
+    }
+
+    @Override
+    public void onClick(final WearableListView.ViewHolder viewHolder) {
+        Log.d("wear","I pressed " + viewHolder.getPosition());
+
+        if (mGoogleApiClient == null)
+            return;
+
+        final PendingResult<NodeApi.GetConnectedNodesResult> nodes = Wearable.NodeApi.getConnectedNodes(mGoogleApiClient);
+        nodes.setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+
+            @Override
+            public void onResult(NodeApi.GetConnectedNodesResult result) {
+                final List<Node> nodes = result.getNodes();
+                if (nodes != null) {
+                    for (int i=0; i<nodes.size(); i++) {
+                        final Node node = nodes.get(i);
+
+                        // You can just send a message
+                        //Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/MESSAGE", null);
+                        switch(viewHolder.getPosition()) {
+                            case 0:
+                                Log.d("wear","pos 0 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/0", null);
+                                break;
+                            case 1:
+                                Log.d("wear","pos 1 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/1", null);
+                                break;
+                            case 2:
+                                Log.d("wear", "pos 2 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/2", null);
+                                break;
+                            case 3:
+                                Log.d("wear", "pos 3 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/3", null);
+                                break;
+                            case 4:
+                                Log.d("wear", "pos 4 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/4", null);
+                                break;
+                            case 5:
+                                Log.d("wear", "pos 5 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/5", null);
+                                break;
+                            case 6:
+                                Log.d("wear", "pos 6 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/6", null);
+                                break;
+                            case 7:
+                                Log.d("wear", "pos 7 pressed");
+                                Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/7", null);
+                                break;
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+
+    }
+
+    private class MyAdapter extends WearableListView.Adapter {
+        private final LayoutInflater mInflater;
+
+        private MyAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new WearableListView.ViewHolder(
+                    mInflater.inflate(R.layout.row_simple_item_layout, null));
+        }
+
+        @Override
+        public void onBindViewHolder(WearableListView.ViewHolder holder, int position) {
+            TextView view = (TextView) holder.itemView.findViewById(R.id.textView);
+            view.setText(listItems.get(position).toString());
+            holder.itemView.setTag(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return listItems.size();
+        }
+    }
+}
+                           /* package tv.piratemedia.lightcontroler;
 
 
 import android.app.Activity;
@@ -88,7 +241,7 @@ public class MainActivity extends Activity {
                                 break;
                         }
                         // or you may want to also check check for a result:
-                       /* final PendingResult<MessageApi.SendMessageResult> pendingSendMessageResult = Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/MESSAGE", null);
+                        final PendingResult<MessageApi.SendMessageResult> pendingSendMessageResult = Wearable.MessageApi.sendMessage(mGoogleApiClient, node.getId(), "/MESSAGE", null);
                         pendingSendMessageResult.setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
                               public void onResult(MessageApi.SendMessageResult sendMessageResult) {
                                   if (sendMessageResult.getStatus().getStatusCode()== WearableStatusCodes.SUCCESS) {
@@ -100,10 +253,11 @@ public class MainActivity extends Activity {
                                        Log.d("wear", "Not succesfully sent");
                                   }
                               }
-                         });*/
-                    }
-                }
-            }
-        });
-    }
-}
+                         });
+                            }
+                                    }
+                                    }
+                                    });
+                                    }
+                                    } */
+
